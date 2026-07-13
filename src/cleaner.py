a51c9a1e2a20073af62c415f1018922dce5ec5d1
Zahra Hemmati <ZahraHemmati@rabin.local>
2026-07-13 23:49:23 +0330

    Add Persian text cleaning module

--- src/cleaner.py
+++ src/cleaner.py
@@ -7,9 +7,8 @@ class TextCleaner:
 
         text = text.replace('ي', 'ی')
         text = text.replace('ك', 'ک')
-
+        # delete extra spaces
         text = re.sub(r'[ \t]+', ' ', text)
-
         text = re.sub(r'\n\s*\n+', '\n\n', text)
 
         return text.strip()
